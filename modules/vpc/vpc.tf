0981a4bfa86fdfcaee48052377a362ff7f30d821
Michael Arnold <github@razorsedge.org>
2020-03-12 23:20:18 -0400

    Set the route to the nat_gateway.
    Prevents constant churn on apply.

--- modules/vpc/vpc.tf
+++ modules/vpc/vpc.tf
@@ -140,7 +140,7 @@ resource "aws_route_table" "private_routes" {
     vpc_id = aws_vpc.the_vpc.id
     route {
         cidr_block = "0.0.0.0/0"
-        gateway_id = aws_nat_gateway.nat_gws[count.index].id
+        nat_gateway_id = aws_nat_gateway.nat_gws[count.index].id
     }
     tags = {
         Name = "${var.PREFIX}cdp-private-route-table-${count.index}"

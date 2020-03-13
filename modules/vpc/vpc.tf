d8e41218432e3afd0a6832eb224de1fa15f360df
Michael Arnold <github@razorsedge.org>
2020-03-12 23:21:07 -0400

    Set aws_vpc cidr_block to the expected variable.

--- modules/vpc/vpc.tf
+++ modules/vpc/vpc.tf
@@ -56,7 +56,7 @@ variable "AZs" {
 resource "aws_vpc" "the_vpc" {
     enable_dns_support = "true"
     enable_dns_hostnames = "true"
-    cidr_block = "10.0.0.0/16"
+    cidr_block = var.VPC_CIDR
 
     tags = {
         Name = "${var.PREFIX}cdp-vpc"
